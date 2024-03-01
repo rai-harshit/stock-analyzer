65d2ff3571109796cbc3e37893ca0c6044ce5807
Harshit Rai <harshit@uw.edu>
2024-03-01 05:08:34 +0000

    Add data processing function

--- dags/stock-analyzer.py
+++ dags/stock-analyzer.py
@@ -16,12 +16,15 @@ default_args = {
     start_date=datetime(2024, 2, 28),
     schedule_interval="@daily"
 )
-def fetch_data():
+def stock_analyzer():
+    import pandas as pd
+    import numpy as np
+    import json
     def call_api_and_save_response(api_url, output_file):
         try:
             # Making the API call
             response = requests.get(api_url)
-            response.raise_for_status()  # Raise an exception for bad status codes
+            response.raise_for_status()
 
             # Storing the response JSON in a file
             with open(output_file, 'w') as f:
@@ -32,6 +35,17 @@ def fetch_data():
             print("Error making API call:", e)
 
     def process_data(input_file, output_file):
+        print(f"Reading {input_file} for processing.")
+        with open(input_file) as f:
+            json_data = json.load(f)
+            time_series_data = json_data["Time Series (Daily)"]
+            df = pd.DataFrame(time_series_data).T
+            df.columns = ['open', 'high', 'low', 'close', 'volume']
+            df = df.astype(float)
+            df.index = pd.to_datetime(df.index)
+            df.reset_index(inplace=True)
+            df.rename(columns={'index': 'date'}, inplace=True)
+            print(df.head())
         print(f"Data processed from {input_file} and written to {output_file}")
     
 
@@ -63,8 +77,8 @@ def fetch_data():
         "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IRCTC.BSE&outputsize=full&apikey=XGLSH8DND0HHFWKK",
         "/opt/airflow/data/irctc_data.json"
     )
-    process_bse_data_task = process_bse_data("/opt/airflow/data/base_data.json","/opt/airflow/data/bse_processed.csv")
-    process_irctc_data_task = process_irctc_data("/opt/airflow/data/base_data.json","/opt/airflow/data/irctc_processed.csv")
+    process_bse_data_task = process_bse_data("/opt/airflow/data/bse_data.json","/opt/airflow/data/bse_processed.csv")
+    process_irctc_data_task = process_irctc_data("/opt/airflow/data/irctc_data.json","/opt/airflow/data/irctc_processed.csv")
 
     confirm_run_task = confirm_run()
 
@@ -73,5 +87,5 @@ def fetch_data():
     process_bse_data_task.set_downstream(confirm_run_task)
     process_irctc_data_task.set_downstream(confirm_run_task)
 
-fetch_data_status = fetch_data()
+fetch_data_status = stock_analyzer()
 
